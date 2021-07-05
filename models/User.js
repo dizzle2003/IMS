f21b8cd2ad86531642a975f3b6b1f1db5b2a83ae
Dennis Ajalie <ajaliedennis@yahoo.com>
2021-07-05 06:56:26 -0600

    changed userschema jwt function to arrow syntax

--- models/User.js
+++ models/User.js
@@ -44,7 +44,7 @@ UserSchema.pre("save", async function (next) {
 });
 
 //Generating web token
-UserSchema.methods.getSignedJwtToken = function () {
+UserSchema.methods.getSignedJwtToken = () => {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES,
   });
