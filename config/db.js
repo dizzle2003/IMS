1a2ffd4fc7eaa20f27a3bd9be81affff35650e2d
Dennis Ajalie <ajaliedennis@yahoo.com>
2022-01-10 09:45:45 -0700

    updated colors

--- config/db.js
+++ config/db.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-    useCreateIndex: true,
-  });
+  const conn = await mongoose.connect(process.env.DB_URI);
 
   console.log(`Mongo Connected on ${conn.connection.host}`);
 };
